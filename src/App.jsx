c46e23ca533692368b33fb16ffac6ac7682f070d
VladislavGodi <ladislavkorrus@yandex.ru>
2024-08-31 15:28:59 +0400

    Добавление интерфейса

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import './App.css';
 
 function App() {
     return (
-        <>
-            <div></div>
-        </>
+      <div className='notebook-container'>
+        <div className="notebook">
+            <h1>Заметки</h1>
+            <div className="noteInput">
+                <textarea id="note-text" placeholder="Напиши что нибудь..."></textarea>
+                <button>Добавить заметку</button>
+            </div>
+            <div className="notes-list">
+                <h2>Список заметок</h2>
+                <ul id="notes"></ul>
+                <li>gsdgsdgs</li>
+            </div>
+        </div>
+        </div>
     );
 }
 
